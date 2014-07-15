5580cf246068f6f261a33c63a35acceeff23c623
23333 <zhou_bw@yeah.net>
2014-07-15 23:36:27 +0800

    no message

--- g/js/main.js
+++ g/js/main.js
@@ -1,5 +1,5 @@
 /*******************清空上一次内容******************/
-	$('body')[0].innerHTML='<audio src="music/Pilgrims on a Long Journey.mp3.mp3" autoplay="autoplay" loop="loop">';
+	$('body')[0].innerHTML='<audio src="music/PilgrimsOnALongJourney.mp3.mp3" autoplay="autoplay" loop="loop"></audio>';
 	if (timer) window.clearInterval(timer);
 
 /*******************添加canvas**********************/
